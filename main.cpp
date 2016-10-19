d9dfc066b9e6397a0d4c4d62c5e909179d31942d
Trifurion <john.j@trifurion.com>
2016-10-18 20:54:40 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+//Goal: Function that gives out buffers. Buffer Size of (2^x)-1 from 7 words to 511 words ( word = 4 bytes )
+//Avail memory of buffer manager will be set to 10 of the maximum buffer size of 511 Words
+//requestor provides a block size and routine will return address of buffer or -1 if not enough space
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    cout << ""
+    return 0;
+}
